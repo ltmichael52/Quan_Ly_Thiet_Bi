9c66d551e7d06cbc7871f863f70d4b9fe1f41eba
ltmichael52 <ltmichael52@gmail.com>
2024-02-01 10:17:56 +0700

    delete code

--- ThietBiDienTu/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ThietBiDienTu.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
