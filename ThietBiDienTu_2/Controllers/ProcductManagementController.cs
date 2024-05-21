5a6ae07b19672b7d37482f62cb509dcb79fc3d65
Le Michael <101556054+ltmichael52@users.noreply.github.com>
2024-05-21 14:40:00 +0700

    P update ajax Cart

--- ThietBiDienTu_2/Controllers/ProcductManagementController.cs
+++ ThietBiDienTu_2/Controllers/ProcductManagementController.cs
@@ -66,7 +66,7 @@ namespace ThietBiDienTu_2.Controllers
                 {
                     Dongthietbi dongtb = displayList.FirstOrDefault(x => x.Madongtb == item.Madongtb);
                     dongtb.Soluong -= item.Soluong;
-                    if (dongtb.Soluong < 0)
+                    if (dongtb.Soluong <= 0)
                     {
                         displayList.Remove(dongtb);
                     }
@@ -129,9 +129,9 @@ namespace ThietBiDienTu_2.Controllers
             {
                 Dongthietbi dongtb = displayList.FirstOrDefault(x => x.Madongtb == item.Madongtb);
                 dongtb.Soluong -= item.Soluong;
-                if(dongtb.Soluong < 0)
+                if(dongtb.Soluong <= 0)
                 {
-                    viewModel.DongThietBiList.Remove(dongtb);
+                    displayList.Remove(dongtb);
                 }
             }
 
--- ThietBiDienTu_2/Models/ToolDbContext.cs
+++ ThietBiDienTu_2/Models/ToolDbContext.cs
@@ -44,7 +44,7 @@ public partial class ToolDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:eu-az-sql-serv1.database.windows.net,1433;Initial Catalog=d8qxlo1pvtdjnw9;Persist Security Info=False;User ID=uhyx8fmimb7my3b;Password=Quanlythietbi@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False");
+        => optionsBuilder.UseSqlServer("Data Source=MICHAEL;Initial Catalog=QLTHIETBI2;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
